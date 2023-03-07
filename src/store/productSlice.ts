09366490c4f5f2ece91a75c6a0ea8cc3ea7820ca
Yiolrin <yiolrin990930@gmail.com>
2023-03-07 18:57:34 +0900

    api 수정

--- src/store/productSlice.ts
+++ src/store/productSlice.ts
@@ -2,62 +2,64 @@ import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { ProductModel } from '../shares/Types';
 
+const { VITE_MOVIE_API_KEY } = import.meta.env;
+
 export type ProductState = {
-	arrProduct: ProductModel[];
-	isPending: boolean;
+  arrProduct: ProductModel[];
+  isPending: boolean;
 };
 
 const initialState: ProductState = {
-	arrProduct: [
-		{
-			id: 1,
-			name: '',
-			alias: '',
-			price: 0,
-			description: '',
-			size: '',
-			shortDescription: '',
-			quantity: 0,
-			deleted: false,
-			categories: '',
-			relatedProducts: '',
-			feature: true,
-			image: '',
-			count: 0,
-		},
-	],
-	isPending: false,
+  arrProduct: [
+    {
+      id: 1,
+      name: '',
+      alias: '',
+      price: 0,
+      description: '',
+      size: '',
+      shortDescription: '',
+      quantity: 0,
+      deleted: false,
+      categories: '',
+      relatedProducts: '',
+      feature: true,
+      image: '',
+      count: 0,
+    },
+  ],
+  isPending: false,
 };
 const productSlice = createSlice({
-	name: 'product',
-	initialState,
-	reducers: {},
-	extraReducers: (builder) => {
-		builder.addCase(getAllProductApi.pending, (state: ProductState) => {
-			state.isPending = true;
-		});
+  name: 'product',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllProductApi.pending, (state: ProductState) => {
+      state.isPending = true;
+    });
 
-		builder.addCase(
-			getAllProductApi.fulfilled,
-			(state: ProductState, action: PayloadAction<ProductModel[]>) => {
-				state.isPending = false;
-				state.arrProduct = action.payload;
-			}
-		);
-		builder.addCase(
-			getAllProductApi.rejected,
-			(state: ProductState, action) => {}
-		);
-	},
+    builder.addCase(
+      getAllProductApi.fulfilled,
+      (state: ProductState, action: PayloadAction<ProductModel[]>) => {
+        state.isPending = false;
+        state.arrProduct = action.payload;
+      },
+    );
+    builder.addCase(
+      getAllProductApi.rejected,
+      (state: ProductState, action) => {},
+    );
+  },
 });
 
 export const getAllProductApi = createAsyncThunk(
-	'productSlice/getAllProductApi',
-	async () => {
-		const API_URL = 'https://shop.cyberlearn.vn/api/Product';
-		const res = await axios.get(API_URL);
-		return res.data.content;
-	}
+  'productSlice/getAllProductApi',
+  async () => {
+    const API_URL = VITE_MOVIE_API_KEY;
+    const res = await axios.get(API_URL);
+    return res.data.content;
+  },
 );
 
 export const productActions = productSlice.actions;
